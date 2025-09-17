4fbb545f006064ef90d5c586cea50cbe27c8fec2
William Cornejo <williamcornejo@Williams-MacBook-Air-the-scnd.local>
2025-09-17 07:40:38 -0400

    got layers

--- js/map_init.js
+++ js/map_init.js
@@ -1,8 +1,13 @@
 const map = L.map('map', { fullscreenControl: true }).setView([40.74281318841831, -73.92931873140857], 11);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+
+let boroughLayer;
+let precinctLayer;
+let parksLayer;
+const layerControl = L.control.layers(null, {}).addTo(map);
 
 function searchPlace(query) {
   const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`;
@@ -41,26 +46,42 @@ $.getJSON('https://data.cityofnewyork.us/resource/tvpp-9vvx.json?$limit=50', fun
 });
 
 
+
 fetch('data/Borough Boundaries_20250909.geojson')
   .then(res => res.json())
   .then(geojson => {
-    L.geoJSON(geojson, {
+    boroughLayer = L.geoJSON(geojson, {
       style: {
         color: '#3366cc',
         weight: 2,
         fillOpacity: 0.1
       }
     }).addTo(map);
-  })
+    layerControl.addOverlay(boroughLayer, 'Borough Boundaries');
+  });
 
-fetch('data/NYC Community Boards_20250909.geojson')
+  fetch('data/Police Precincts_20250909.geojson')
   .then(res => res.json())
   .then(geojson => {
-    L.geoJSON(geojson, {
-      style: {
-        color: '#3366cc',
-        weight: 2,
-        fillOpacity: 0.1
+    precinctLayer = L.geoJSON(geojson, {
+        style: {
+        color: '#cc3333',
+        weight: 3,
+        fillOpacity: 0.05
       }
     }).addTo(map);
-  })
+    layerControl.addOverlay(precinctLayer, 'Police Precincts');
+  });
+  
+  fetch('data/Parks Properties_20250916.geojson')
+  .then(res => res.json())
+  .then(geojson => {
+    boroughLayer = L.geoJSON(geojson, {
+      style: {
+        color: '#33cc3dff',
+        weight: 2
+      }
+    }).addTo(map);
+    layerControl.addOverlay(boroughLayer, 'Parks Properties');
+  });
+
